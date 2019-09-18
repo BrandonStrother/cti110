de32298567fa7f68088e9466ebbabdcd668785c8
BrandonStrother <55504243+BrandonStrother@users.noreply.github.com>
2019-09-18 12:23:40 -0400

    Add files via upload
    First Submisson

---
+++ P2T1_StrotherBrandon.py
@@ -0,0 +1,11 @@
+#Tutorial video project
+#9/16/19
+#CTI-110 P2T1 - Sales Prediction
+#Brandon Strother
+#
+
+totalSales = float(input("Enter the projected sales: "))
+
+profit = (totalSales*0.23)
+
+print ("The Profit is $", format(profit, ".2f"))
